ac1c76e91b4a3a525334f20898bf04e322f324d7
Johannes Roth <johannes.roth@aol.com>
2015-04-29 17:47:02 +0200

    Added FPS counter

--- src/com/naronco/pedopac/Main.java
+++ src/com/naronco/pedopac/Main.java
@@ -14,17 +14,27 @@ public class Main {
 			e.printStackTrace();
 			System.exit(0);
 		}
-		
+
 		Game game = new Game();
-		
+
 		long time = System.currentTimeMillis();
+
+		long lastFinishedSecond = System.currentTimeMillis();
+		int frames = 0;
 
 		while (!Display.isCloseRequested()) {
 			long now = System.currentTimeMillis();
-			
+
 			game.update((now - time) * 0.001f);
 			game.render();
-			
+			++frames;
+
+			if ((now - lastFinishedSecond) >= 1000) {
+				System.out.println(frames + " FPS");
+				frames = 0;
+				lastFinishedSecond = now;
+			}
+
 			Display.update();
 			time = now;
 		}
