922ecc89d1af6b7fb7570a9e9d519fed9d7692c0
Johannes Roth <johannes.roth@aol.com>
2015-05-02 17:36:16 +0200

    Added Mesh and Vertex class

---
+++ src/com/naronco/pedopac/Mesh.java
@@ -0,0 +1,37 @@
+package com.naronco.pedopac;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class Mesh {
+	private int displayList;
+
+	public Mesh(Vertex[] vertices, int[] indices) {
+		displayList = glGenLists(1);
+
+		glNewList(displayList, GL_COMPILE);
+		glBegin(GL_TRIANGLES);
+
+		for (int index : indices) {
+			Vertex vertex = vertices[index];
+			glTexCoord2f(vertex.getUv().x, vertex.getUv().y);
+			glNormal3f(vertex.getNormal().x, vertex.getNormal().y,
+					vertex.getNormal().z);
+			glColor4f(vertex.getColor().x, vertex.getColor().y,
+					vertex.getColor().z, vertex.getColor().w);
+			glVertex3f(vertex.getPosition().x, vertex.getPosition().y,
+					vertex.getPosition().z);
+		}
+
+		glEnd();
+		glEndList();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		glDeleteLists(displayList, 1);
+	}
+
+	public void render() {
+		glCallList(displayList);
+	}
+}
---
+++ src/com/naronco/pedopac/Vertex.java
@@ -0,0 +1,71 @@
+package com.naronco.pedopac;
+
+import org.lwjgl.util.vector.*;
+
+public class Vertex {
+	private Vector3f position;
+	private Vector2f uv;
+	private Vector3f normal;
+	private Vector4f color;
+
+	public Vertex(Vector3f position) {
+		this.position = position;
+		this.uv = new Vector2f(0, 0);
+		this.normal = new Vector3f(0, 0, 1);
+		this.color = new Vector4f(1, 1, 1, 1);
+	}
+
+	public Vertex(Vector3f position, Vector2f uv) {
+		this.position = position;
+		this.uv = uv;
+		this.normal = new Vector3f(0, 0, 1);
+		this.color = new Vector4f(1, 1, 1, 1);
+	}
+
+	public Vertex(Vector3f position, Vector2f uv, Vector3f normal) {
+		this.position = position;
+		this.uv = uv;
+		this.normal = normal;
+		this.color = new Vector4f(1, 1, 1, 1);
+	}
+
+	public Vertex(Vector3f position, Vector2f uv, Vector3f normal,
+			Vector4f color) {
+		this.position = position;
+		this.uv = uv;
+		this.normal = normal;
+		this.color = color;
+	}
+
+	public void setPosition(Vector3f position) {
+		this.position = position;
+	}
+
+	public void setUv(Vector2f uv) {
+		this.uv = uv;
+	}
+
+	public void setNormal(Vector3f normal) {
+		this.normal = normal;
+	}
+
+	public void setColor(Vector4f color) {
+		this.color = color;
+	}
+
+	public Vector3f getPosition() {
+		return position;
+	}
+
+	public Vector2f getUv() {
+		return uv;
+	}
+
+	public Vector3f getNormal() {
+		return normal;
+	}
+
+	public Vector4f getColor() {
+		return color;
+	}
+}
